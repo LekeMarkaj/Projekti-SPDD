925afab1c378ca2d2afb2dd816012866d8bd09b1
LekeMarkaj <markaj.leka@gmail.com>
2024-11-03 17:26:41 +0100

    0

---
+++ Web/frontend/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+export default function App() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to My Simple React Page</h1>
+      </header>
+      <p>This is a simple React page example.</p>
+      <button>BUTTON</button>
+    </div>
+  );
+}
+
+// Select the root element by ID and render the App component inside it
+const appDiv = document.getElementById("app");
+const root = createRoot(appDiv);
+root.render(<App />);
